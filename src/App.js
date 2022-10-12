7aafcae9428ee5d7ff2bff0098a238855ff7d39f
ibro001 <ibrahimmoses56@gmail.com>
2022-10-12 22:13:35 +0100

    created the pages route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/register' element = { <Register /> } />
+        <Route path = '/login' element = { <Login /> } />
+        <Route path = '/' element = { <Chat /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
